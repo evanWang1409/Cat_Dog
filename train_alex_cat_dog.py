1ff1ec14c31067bd1b5e9a8af6d5044f9fdec6f0
Ziyi Wang <zw119@duke.edu>
2018-09-17 20:43:31 -0400

    Change dataloader
    Directly read from folder(folder name is ground truth)

--- train_alex_cat_dog.py
+++ train_alex_cat_dog.py
@@ -1,27 +1,33 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
-
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-trainset = torchvision.datasets.CIFAR10(root='./cifar_data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./cifar_data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, models, transforms
+from matplotlib import pyplot as plt
+import os
+
+
+data_transforms = transforms.Compose(
+    [transforms.Resize(256),
+     transforms.CenterCrop(227),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+train_data_dir = '/Users/evnw/Research/Cats_v_Dogs/data/train-by-class'
+train_dataset = datasets.ImageFolder(train_data_dir,
+                                          data_transforms)
+
+trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=4,
+                                             shuffle=True, num_workers=4)
+
+dataset_sizes = len(train_dataset)
+
+classes = train_dataset.classes
+print(classes)
+
 
 
 class AlexNet(nn.Module):
@@ -43,7 +49,15 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
-        self.classifier = nn.Linear(256, num_classes)
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
 
     def forward(self, x):
         x = self.features(x)
@@ -55,7 +69,6 @@ class AlexNet(nn.Module):
 net = AlexNet()
 
 
-import torch.optim as optim
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
@@ -73,6 +86,7 @@ for epoch in range(2):  # loop over the dataset multiple times
         optimizer.zero_grad()
 
         # forward + backward + optimize
+        print(inputs.size())
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
@@ -92,3 +106,5 @@ for epoch in range(2):  # loop over the dataset multiple times
     	break
 
 torch.save(net.state_dict(), 'alex_cifar10.pt')
+
+
