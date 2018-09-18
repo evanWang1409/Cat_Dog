12ee0b52e9116e0e8f1f6b5975ff2b9d6deb26b9
evanWang1409 <zw119@duke.edu>
2018-09-18 18:03:27 -0400

    little change

--- FGSM-attack.py
+++ FGSM-attack.py
@@ -16,9 +16,6 @@ from collections import Iterable
 from scipy.stats import truncnorm
 
 def to_var(x, requires_grad=False, volatile=False):
-    """
-    Varialbe type that automatically choose cpu or cuda
-    """
     if torch.cuda.is_available():
         x = x.cuda()
     return Variable(x, requires_grad=requires_grad, volatile=volatile)
@@ -50,9 +47,47 @@ class FGSMAttack(object):
         scores = self.model(X_var)
         loss = self.loss_fn(scores, y_var)
         loss.backward()
+
+        # attack function
         grad_sign = X_var.grad.data.cpu().sign().numpy()
 
         X += self.epsilon * grad_sign
         X = np.clip(X, 0, 1)
 
         return X
+
+class AlexNet(nn.Module):
+
+    def __init__(self, num_classes=10):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
