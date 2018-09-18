5144031dd2e964543baab27a9ad1adaed29833ef
Ziyi Wang <zw119@duke.edu>
2018-09-18 12:04:22 -0400

    finish test file

--- test_alex_cat_dog.py
+++ test_alex_cat_dog.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from torchvision import datasets, models, transforms
 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -16,39 +17,42 @@ data_transforms = transforms.Compose(
      transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
+test_data_dir = '/Users/evnw/Research/Cats_v_Dogs/data/train_by_class'
 
-test_data_dir = '/Users/evnw/Research/Cats_v_Dogs/data/train-by-class'
-test_dataset = datasets.ImageFolder(train_data_dir,
-                                          data_transforms)
+test_dataset = datasets.ImageFolder(test_data_dir, data_transforms)
+testloader = torch.utils.data.DataLoader(test_dataset, batch_size=4, shuffle=True, num_workers=4)
 
-trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=4,
-                                             shuffle=True, num_workers=4)
-
-dataset_sizes = len(train_dataset)
-
-classes = train_dataset.classes
-print(classes)
+dataset_sizes = len(test_dataset)
+classes = test_dataset.classes
 
 class AlexNet(nn.Module):
 
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=5),
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
         )
-        self.classifier = nn.Linear(256, num_classes)
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
 
     def forward(self, x):
         x = self.features(x)
@@ -57,9 +61,7 @@ class AlexNet(nn.Module):
         return x
 
 net = AlexNet()
-
-net.load_state_dict(torch.load('alex_cat_dog_iter100000.pt'))
-
+net.load_state_dict(torch.load('alex_cat_dog_iter10000.pt'))
 
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
@@ -71,25 +73,32 @@ def imshow(img):
 dataiter = iter(testloader)
 images, labels = dataiter.next()
 
+""" sample display
 # print images
-#imshow(torchvision.utils.make_grid(images))
-#plt.show()
+imshow(torchvision.utils.make_grid(images))
+plt.show()
 outputs = net(images)
 print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 _, predicted = torch.max(outputs, 1)
 print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
                               for j in range(4)))
-
+"""
 
 correct = 0
 total = 0
 with torch.no_grad():
+    count = 0
     for data in testloader:
         images, labels = data
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
+        count += 1
+        if count%100 == 0:
+            print(count)
+        if(count == 2000):
+            break
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (
+print('Accuracy of the network on the 2000 test images: %d %%' % (
     100 * correct / total))
